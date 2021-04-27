feb6f6e2adcd9cec7593c24d664882440f423782
nannapatm <mm.nannapat@gmail.com>
2021-04-27 09:38:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nfc_java/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.nfc_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enableReaderMode();
+    }
+
+    private void enableReaderMode() {
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
+        if (nfc != null) {
+            nfc.enableReaderMode(this, new NFCCardReader(this), NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, null);
+        }
+    }
+
+    public void displayTagId(final String tagId) {
+        final TextView txtTagId = (TextView) findViewById(R.id.result);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtTagId.setText(tagId);
+            }
+        });
+    }
+}
+
